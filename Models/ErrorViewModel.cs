47d5c33877ef92dd6734b9548fde3b5c55f475cb
Arbross <victor.pavlovskyi@gmail.com>
2021-12-24 18:26:27 +0200

    First load

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Exam_ASP_NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
